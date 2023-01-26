407d6921863a04d1f81f50e3bf54c24ddfea2fd1
Nakanishi125 <nakanishi.y125@gmail.com>
2023-01-26 17:05:19 +0900

    修論本提出

--- calculation/3_3_3/makegraph_revrrt.py
+++ calculation/3_3_3/makegraph_revrrt.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 fig = plt.figure()
-ax = fig.add_subplot(111, xlim=(0, 500000), ylim=(0, 3000),xlabel='The number of iterations', ylabel='The size of C_free_obj')
+ax = fig.add_subplot(111, xlim=(0, 500000), ylim=(0, 2e6), xlabel='The number of iterations', ylabel='The size of C_free_obj [mm^2 deg]')
 fn = ['data1.csv','data2.csv','data3.csv','data4.csv','data5.csv']
    
 for f in range(len(fn)):
@@ -16,12 +16,14 @@ for f in range(len(fn)):
         if i != n-1:
             add = np.array([data[2*i+1, 0]-1, data[2*i, 1]])
             data = np.insert(data, 2*i+1, add, axis=0)
-    
+   
     x = data[:, 0]
     y = data[:, 1]
+    for i in range(len(y)):
+        y[i] = y[i]*500
     
     
     ax.plot(x, y,label=f)
 
 
-fig.savefig("transition.eps")
+fig.savefig("transition_vol.eps")
---
+++ calculation/make_boxplot.py
@@ -0,0 +1,37 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy as np 
+
+df_data = pd.read_csv("Summerize.csv")
+
+data_xyz = df_data.loc[:9, '3_2_1_xyz'].to_numpy()
+data_yz = df_data.loc[:9, '3_2_1_yz']
+
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+data1 = (data_xyz, data_yz)
+ax1.boxplot(data1, sym='')
+ax1.grid()
+ax1.set_xticklabels(['Previous goal', 'Relieved goal'], fontsize=14)
+ax1.set_ylabel('Calculation time [s]', fontsize=14)
+fig1.savefig('3_2_1.eps')
+
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111, ylabel='Calculation time [s]')
+data2 = (df_data.loc[:9, '3_2_2fullscan'], df_data.loc[:9, '3_2_2efficient'])
+ax2.boxplot(data2, sym='')
+ax2.grid()
+ax2.set_xticklabels(['Fullscan and\nclustering', 'Efficient\nextraction'], fontsize=14)
+ax2.set_ylabel('Calculation time [s]', fontsize=14)
+fig2.savefig('3_2_2.eps')
+
+
+fig3 = plt.figure()
+ax3 = fig3.add_subplot(111, ylabel='Calculation time [s]')
+data3 = (df_data.loc[:, '3_4_1Forward'], df_data.loc[:, '3_4_1Reverse'], df_data.loc[:, '3_4_1Connect'])
+ax3.boxplot(data3, sym='')
+ax3.grid()
+ax3.set_xticklabels(['Forward\nsearch', 'Reverse\nsearch', 'Bidirectional\nsearch'], fontsize=14)
+ax3.set_ylabel('Calculation time [s]', fontsize=14)
+fig3.savefig('3_4_1.eps')
